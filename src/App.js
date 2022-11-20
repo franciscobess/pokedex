1b12a1a88277baa9a4c15fb3697de3ad4823657b
franciscobess <edenilson.victoralves@outlook.com>
2022-11-20 03:19:16 -0300

    Creating the skelleton of the project with react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Pikachu from './assets/sprites/pikachu.jpg'
+import PokedexLogo from './assets/pokedex-logo.png'
+
+function App() {
+  return (
+
+    <div className="App">
+      <div className='pokemon-logo-container'>
+        <img src={PokedexLogo} alt="Pokedex Logo" className='pokedex-logo-img' />
+      </div>
+
+      <div className='pokemon-info-container'>
+        <div className='pokemon-sprite'>
+          <img src={Pikachu} className='pokemon-sprite-img' alt="Pokemon Sprite" />
+        </div>
+
+        {/* <div className='pokedex-description'>
+          <h2>Descrição</h2>
+        </div> */}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
